5a74e2d289c585b2cc7aa20b25d970c76a3a27fb
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2021-11-21 12:51:50 +0100

    Simple string and cin.

---
+++ example2/example2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+
+	std::cout << "Podaj imie: ";
+	std::string imie;
+	std::cin >> imie;
+	std::cout << "Podaj nazwisko: ";
+	std::string nazwisko;
+	std::cin >> nazwisko;
+	std::cout << "Witaj " << imie << " " << nazwisko << std::endl;
+
+}
