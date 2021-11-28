5986cb8e04e848259c5f2193706339e9d96dfbcd
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2021-11-28 08:23:32 +0100

    Task with creating simple Class.

---
+++ rectangle/rectangle.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+class Rectangle
+{
+private:
+    double width;
+    double height;
+public:
+    Rectangle();
+    Rectangle(double side);
+    Rectangle(double width, double height);
+    double getWidth();
+    double getHeight();
+    double getDiagonal();
+    double getArea();
+    bool isLargerThan(const Rectangle&);
+    void info();
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+Rectangle::Rectangle() : Rectangle(1)
+{
+}
+
+Rectangle::Rectangle(double side) : Rectangle(side, side)
+{
+}
+
+Rectangle::Rectangle(double width, double height) {
+    this->width = width;
+    this->height = height;
+}
